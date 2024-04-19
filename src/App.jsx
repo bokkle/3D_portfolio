7985fb24e71677bade2708d1f3a1c816336418a6
bokkle <mitch.r_@hotmail.com>
2024-04-18 23:36:15 -0400

    initial

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+
+const App = () => {
+  return (
+    <main className="bg-slate-300/20">
+      <Router>
+        Navbar
+        <Routes>
+          <Route path="/" element={"Home"} />
+          <Route path="/about" element={"About"} />
+        </Routes>
+      </Router>
+    </main>
+  );
+};
+
+export default App;
